ce9b5527eefce8c87cd3f4843cfcea4d277077a5
Andrew Wormald <andreww@luno.com>
2019-10-11 12:05:36 +0200

    wip

--- skunk/ops/consumers.go
+++ skunk/ops/consumers.go
@@ -2,13 +2,13 @@ package ops
 
 import (
 	"context"
-	"github.com/luno/reflex"
-
 	"github.com/luno/fate"
+	"github.com/luno/reflex"
 )
 
 func makeConsume(b Backends) reflex.Consumer {
 	fn := func(ctx context.Context, fate fate.Fate, e *reflex.Event) error {
+
 		return fate.Tempt()
 	}
 
