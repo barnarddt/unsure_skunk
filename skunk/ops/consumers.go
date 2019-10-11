ebaa88dffe97712c7fa6e93ed48caf26f1ab1343
Andrew Wormald <andreww@luno.com>
2019-10-11 14:27:32 +0200

    wip

--- skunk/ops/consumers.go
+++ skunk/ops/consumers.go
@@ -5,22 +5,26 @@ import (
 	"github.com/luno/fate"
 	"github.com/luno/reflex"
 	"unsure_skunk/skunk"
+	"unsure_skunk/skunk/db/rounds"
 )
 
-func makeConsume(b Backends) reflex.Consumer {
+func makeConsume(b Backends, c skunk.Client) reflex.Consumer {
 	fn := func(ctx context.Context, fate fate.Fate, e *reflex.Event) error {
 		if !reflex.IsAnyType(e.Type,
 			skunk.RoundStatusJoined,
 			skunk.RoundStatusCollected,
 			skunk.RoundStatusSubmitted,
-			skunk.RoundStatusSuccess,
-			skunk.RoundStatusFailed,
 		) {
 			return fate.Tempt()
 		}
 
-		if !reflex.IsAnyType(e.Type, skunk.RoundStatusCollected) {
+		if !reflex.IsType(e.Type, skunk.RoundStatusCollected) {
 			// fetch parts from e.ForeignID
+			//part, err := parts.Lookup(ctx, b.SkunkDB(), e.ForeignIDInt())
+			r, err := rounds.Lookup(ctx, b.SkunkDB(),e.ForeignID)
+
+
+			c.GetParts(ctx,  r. int64, player string)
 		}
 
 		return fate.Tempt()
