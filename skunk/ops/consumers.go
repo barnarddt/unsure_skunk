1fb127871c07ddeab9dd314972029d2bac0ae935
Andrew Wormald <andreww@luno.com>
2019-10-11 12:18:53 +0200

    wip

--- skunk/ops/consumers.go
+++ skunk/ops/consumers.go
@@ -4,10 +4,28 @@ import (
 	"context"
 	"github.com/luno/fate"
 	"github.com/luno/reflex"
+	"unsure_skunk/skunk"
 )
 
 func makeConsume(b Backends) reflex.Consumer {
 	fn := func(ctx context.Context, fate fate.Fate, e *reflex.Event) error {
+		if !reflex.IsAnyType(e.Type,
+			skunk.RoundStatusJoin,
+			skunk.RoundStatusJoined,
+			skunk.RoundStatusCollect,
+			skunk.RoundStatusCollected,
+			skunk.RoundStatusSubmit,
+			skunk.RoundStatusSubmitted,
+			skunk.RoundStatusSuccess,
+			skunk.RoundStatusFailed,
+			skunk.RoundStatusSubmit,
+		) {
+			return fate.Tempt();
+		}
+
+		if reflex.IsType(e, skunk.RoundStatusJoin) {
+
+		}
 
 		return fate.Tempt()
 	}
