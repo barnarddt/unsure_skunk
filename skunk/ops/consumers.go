31d9fb23212e26dadb731070c0a59d729230e6f8
Andrew Wormald <andreww@luno.com>
2019-10-11 11:52:42 +0200

    add commented boilerplate

--- skunk/ops/consumers.go
+++ skunk/ops/consumers.go
@@ -1,4 +1,14 @@
 package ops
 
-import (
-)
+//import (
+//	"context"
+//
+//	"github.com/luno/fate"
+//)
+
+//func makeConsumeCloseAccount(b Backends) func(ctx context.Context, fate fate.Fate, event ) error {
+//	return func(ctx context.Context, fate fate.Fate, e *) error {
+//
+//		return fate.Tempt()
+//	}
+//}
