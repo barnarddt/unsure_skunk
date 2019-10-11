5e623d55409e0c0f913d6b9a822007ec10c8ddbc
barnard <bdutoit@luno.com>
2019-10-11 12:07:48 +0200

    Start match

--- skunk/ops/loops.go
+++ skunk/ops/loops.go
@@ -2,9 +2,13 @@ package ops
 
 import (
 	"context"
+	"time"
 
 	"github.com/corverroos/unsure"
+	"github.com/corverroos/unsure/engine"
 	"github.com/luno/fate"
+	"github.com/luno/jettison/errors"
+	"github.com/luno/jettison/log"
 	"github.com/luno/reflex"
 
 	"unsure_skunk/skunk"
@@ -15,6 +19,8 @@ func StartLoops(b Backends) {
 	for _, peer := range b.GetPeers() {
 		go consumePeerEvents(b, peer)
 	}
+
+	go startMatchForever(b)
 }
 
 func consumePeerEvents(b Backends, peer skunk.Client) {
@@ -22,6 +28,26 @@ func consumePeerEvents(b Backends, peer skunk.Client) {
 		cursors.ToStore(b.SkunkDB().DB))
 	consumer := Dummy(b)
 	unsure.ConsumeForever(unsure.FatedContext, consumable.Consume, consumer)
+}
+
+func startMatchForever(b Backends) {
+	for {
+		ctx := unsure.ContextWithFate(context.Background(), unsure.DefaultFateP())
+
+		err := b.EngineClient().StartMatch(ctx, "skunkworx", len(b.GetPeers()))
+
+		if errors.Is(err, engine.ErrActiveMatch) {
+			// Match active, just ignore
+			return
+		} else if err != nil {
+			log.Error(ctx, errors.Wrap(err, "start match error"))
+		} else {
+			log.Info(ctx, "match started")
+			return
+		}
+
+		time.Sleep(time.Second)
+	}
 }
 
 func Dummy(backends Backends) reflex.Consumer {
